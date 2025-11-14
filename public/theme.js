f80dbc6895d705617e0d50cfc25fc80c0af46521
Catskill909 <36337311+Catskill909@users.noreply.github.com>
2025-11-14 12:22:42 -0500

    eye icon for password fields

--- public/theme.js
+++ public/theme.js
@@ -43,3 +43,19 @@
         createThemeToggle();
     }
 })();
+
+// Password Visibility Toggle
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const toggle = input.parentElement.querySelector('.password-toggle i');
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        toggle.classList.remove('fa-eye');
+        toggle.classList.add('fa-eye-slash');
+    } else {
+        input.type = 'password';
+        toggle.classList.remove('fa-eye-slash');
+        toggle.classList.add('fa-eye');
+    }
+}
